b7a43e40f65f1325c169ff749356d348f38980e3
X极限战士X <1014723833@qq.com>
2019-12-24 23:17:51 +0800

    修改水炮出力差值

--- JoystickMod/Block/Blocks/WaterCannonScript.cs
+++ JoystickMod/Block/Blocks/WaterCannonScript.cs
@@ -10,33 +10,28 @@ class WaterCannonScript : Block
     AnimationCurve animationCurve;
 
     bool StrengthSliderValueSign = false;
-
+    float baseStrength = 1f;
     WaterCannonController waterCannonController;
     public override void OnSimulateStart_Enable()
     {
         waterCannonController = GetComponent<WaterCannonController>();
 
-        animationCurve = new AnimationCurve(new Keyframe[] { new Keyframe(0f /*+ joyAxis.CurveValue*/, 0f), new Keyframe(1f */* joyAxis.Max*/CurveMax, waterCannonController.StrengthSlider.Value) });
+        animationCurve = new AnimationCurve(new Keyframe[] { new Keyframe(0f, 0f), new Keyframe(CurveMax, waterCannonController.StrengthSlider.Value) });
 
         StrengthSliderValueSign = waterCannonController.StrengthSlider.Value > 0 ? true : false;
+        baseStrength = Mathf.Abs(waterCannonController.StrengthSlider.Value);
     }
     float targetStrength = 0f;
     public override void SimulateUpdateAlways_Enable()
     {
         if (StrengthSliderValueSign)
         {
-            waterCannonController.isActive = /*joyAxis.DirectionValue*/GetAxesValueDirection() > 0 ? true : false;
+            waterCannonController.isActive = GetAxesValueDirection() > 0 ? true : false;
         }
 
         var value = 0f;
-        //if (LerpToggle.IsActive)
-        //{
-        value = Mathf.MoveTowards(targetStrength, animationCurve.Evaluate(/*joyAxis.CurveValue*/GetAxesValue()), Lerp * Mathf.Pow(10, (waterCannonController.StrengthSlider.Value.ToString().Length - 1)));
-        //}
-        //else
-        //{
-        //    value =/* animationCurve.Evaluate(*/joyAxis.Value/*)*/;
-        //}
+        value = Mathf.MoveTowards(targetStrength, animationCurve.Evaluate(GetAxesValue()), Lerp * Mathf.Pow(Mathf.Min(baseStrength * 0.1f, 10f), (waterCannonController.StrengthSlider.Value.ToString().Length - 1)));
+
         targetStrength = value;
 
         waterCannonController.StrengthSlider.Value = targetStrength;
